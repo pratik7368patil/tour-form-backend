c6b68f16dd64915ffb9db1d8d99d8e9d3b69537b
Pratik Patil <pratik7368patil@gmail.com>
2022-06-18 14:49:32 +0530

    New API's added for update and view single tour

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import { connectToServer, getDb } from "./db/conn.js";
+import mongoDB from "mongodb";
+const ObjectId = mongoDB.ObjectId;
 
 const app = express();
 app.use(cors());
@@ -23,6 +25,15 @@ app.get("/view", function (req, res) {
   });
 });
 
+app.get("/view/:id", function (req, res) {
+  const _db = getDb();
+  const id = { _id: ObjectId(req.params.id) };
+  _db.findOne(id, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
 app.post("/insert", function (req, response) {
   const _db = getDb();
   const data = req.body;
@@ -30,6 +41,29 @@ app.post("/insert", function (req, response) {
     if (err) throw err;
     response.statusCode = 200;
     response.json(res);
+  });
+});
+
+app.post("/update", async function (req, response) {
+  const _db = getDb();
+  const newData = { ...req.body };
+  delete newData._id;
+  const res = await _db.updateOne(
+    { _id: ObjectId(req.body._id) },
+    {
+      $set: newData,
+    }
+  );
+  response.send(res);
+});
+
+app.delete("/delete/:id", function (req, response) {
+  const _db = getDb();
+  const id = { _id: ObjectId(req.params.id) };
+
+  _db.deleteOne(id, function (err, obj) {
+    if (err) throw err;
+    response.json(obj);
   });
 });
 
